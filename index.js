070bf925c4879aaa0e7dc83d7db34f7ebacc05ef
pengbin2367 <pengbin2367@163.com>
2024-04-21 10:52:39 +0800

    feat: index

---
+++ index.js
@@ -0,0 +1,24 @@
+const projects = [
+    "001_ExpandingCards",
+    "002_ProgressSteps"
+];
+
+const penList = document.getElementById("penList");
+
+projects.forEach(project => {
+    let pen = document.createElement("li");
+    pen.className = "pen";
+    let frameElement = document.createElement("iframe");
+    frameElement.src = project + "/index.html";
+    let penBottom = document.createElement("div");
+    penBottom.className = "penBottom";
+    let title = document.createElement("a");
+    title.className = "title";
+    title.href = project + "/index.html";
+    title.innerText = project;
+    
+    penBottom.appendChild(title);
+    pen.appendChild(frameElement);
+    pen.appendChild(penBottom);
+    penList.appendChild(pen);
+})
